cc9c11717a23be75f38a53c5f2c427c00d3cf023
William Wang <will.wang8421@gmail.comgit config --global user.email will.wang8421@gmail.com>
2017-10-12 13:01:15 -0700

    added favicon

--- public/index.js
+++ public/index.js
@@ -3,6 +3,6 @@ import { render } from 'react-dom';
 // import { Provider } from 'react-redux';
 // import { createStore, applyMiddleware } from 'redux';
 
-import App from './src/containers/App';
+import App from './src/Containers/App';
 
 render(<App />, document.getElementById('app'));
--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const parser = require('body-parser');
 const PORT = process.env.PORT || 3030;
+const path = require('path');
 
 const app = express()
   .use(parser.json())
   .use(parser.urlencoded({ extended: true }))
   .use(express.static('public'))
   .listen(PORT, err => err ? console.log(`Err in connecting to server: ${err}`) : console.log(`Successfully connected on PORT: ${PORT}`));
+
+  // app.get('/', (req, res) => {
+  //   res.sendFile(path.resove(__dirname + '/public') + '/public/index.html')
+  // })
